3a1444f95593c934b862cda6ea912dd4fcc80306
Honza Linka <dev@janlinka.cz>
2020-12-05 06:56:10 +0100

    remove sort from d05

--- d05/src/main.rs
+++ d05/src/main.rs
@@ -14,7 +14,7 @@ fn find_index(high: char, data: &[char]) -> i64 {
 }
 
 fn main() {
-    let mut numbers: Vec<i64> = fs::read_to_string("inputs/input_d05.txt")
+    let numbers: Vec<i64> = fs::read_to_string("inputs/input_d05.txt")
         .expect("File not found")
         .lines()
         .map(|x| {
@@ -22,16 +22,12 @@ fn main() {
             8 * find_index('B', &row[..7]) + find_index('R', &row[7..])
         })
         .collect();
-    numbers.sort();
-    println!("max id {}", numbers.last().unwrap());
 
-    let mut seen = numbers[0];
-    for n in &numbers[1..] {
-        if n - 1 != seen {
-            println!("my id {}", n - 1);
-            break;
-        } else {
-            seen = *n;
-        }
-    }
+    let min = numbers.iter().min().unwrap().clone();
+    let max = numbers.iter().max().unwrap().clone();
+    println!("max id {}", max);
+    let mut empty = vec![true; (max - min + 1) as usize];
+    numbers.iter().for_each(|x| empty[(x - min) as usize] = false);
+    let empty_id: i64 = empty.iter().position(|seat| *seat).unwrap() as i64 + min;
+    println!("my id {}", empty_id);
 }
